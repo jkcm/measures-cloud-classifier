6ff7860c26f8fc133e1ece48602a2fb11f5c3d6f
Johannes Mohrmann <jkcm@uw.edu>
2019-04-03 14:53:27 -0700

    adding training_dash_app

---
+++ training_dash_app/classification_utils.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import ast
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+
+test_classification_file = r'/home/disk/eos4/jkcm/Data/MEASURES/zooniverse/classification_datasets/classify_128km-classifications_2019-03-28.csv'
+
+def annotations_parser_v3271(annotation_string):
+    annotation_string = annotation_string.replace('"value":null}', '"value":"Other cloud type, not in previous list."}')
+    label_map = {
+        'Solid Stratus (no cells)': 'solid_stratus',
+        'Closed-cellular MCC': 'closed_mcc',
+        'Open-cellular MCC': 'open_mcc',
+        'Disorganized MCC': 'disorg_mcc',
+        'Suppressed cumulus': 'suppressed_cu',
+        'Clustered cumulus': 'clustered_cu',
+        'Mixed/No dominant type': 'bad_no_dominant_type',
+        'Too much sun glint': 'bad_sun_glint',
+        'Too much \'bowtie\' (striping from high viewing angle)': 'bad_bowtie',
+        'Other cloud type, not in previous list.': 'bad_other',
+        'Polar (sea ice\/night time)': 'bad_polar'}
+    parsed_dict = {x['task']: x['value'] for x in ast.literal_eval(annotation_string)}
+    if parsed_dict['T0'] == "Other/no dominant type/can't tell":
+        if not parsed_dict['T2']:
+            print('null value!')
+            return label_map['Other cloud type, not in previous list.']
+        return label_map[parsed_dict['T2']]
+    else:
+        return label_map[parsed_dict['T0']]
+
+def tidy_parser(annotation_string, basic_parser=annotations_parser_v3271):
+    rough_string = basic_parser(annotation_string)
+    nice_map = {'solid_stratus': 'Stratus',
+        'closed_mcc': 'Closed-cell MCC',
+        'open_mcc': 'Open-cell MCC',
+        'disorg_mcc': 'Disorg. MCC',
+        'suppressed_cu': 'Suppressed Cu',
+        'clustered_cu': 'Clustered Cu',
+        'bad_no_dominant_type': 'No Dominant Type',
+        'bad_sun_glint': 'Other Issue',
+        'bad_bowtie': 'Other Issue',
+        'bad_other': 'Other Issue',
+        'bad_polar': 'Other Issue'}
+    return nice_map[rough_string]
+    
+    
+annotations_parser = annotations_parser_v3271
+
+def count_number_of_classifications(data):
+    class_count = {}#{'total': len(data)}
+    for label in set(data['label'].values):
+        class_count[label] = sum(data['label'].values == label)
+    return class_count
+
+def read_and_parse_classifications(csv_file, annotations_parser=annotations_parser):
+    columns_to_ignore = ['gold_standard', 'expert', 'metadata', 'user_ip']
+    data = pd.read_csv(csv_file).drop(columns=columns_to_ignore)
+    data = data.drop(index=np.nonzero(data['workflow_version'].values<32.71)[0])
+    data['label'] = [annotations_parser(i) for i in data['annotations'].values]
+    return data
---
+++ training_dash_app/training_dash_app.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+deploy=False
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+
+import pandas as pd
+import classification_utils as utils
+
+
+if deploy:
+    from flask import Flask
+    server = Flask(__name__)
+    app = dash.Dash('Cloud classification training', server=server)
+else:        
+    app = dash.Dash('Cloud classification training')
+
+app.title = 'CUMULO Training Tracker | Dash'
+
+data = utils.read_and_parse_classifications(utils.test_classification_file, annotations_parser=utils.tidy_parser)
+users = list(set(data.user_name.values))
+
+all_label_counts = data.groupby('label').count().annotations
+
+overview_figure ={
+                'data': [
+                    go.Bar(
+                        x=all_label_counts.index.values,
+                        y=all_label_counts.values,
+                        text=['{:.1%} of 1000 sample goal'.format(i) for i in all_label_counts.values/1000],
+                    )
+                ],
+                'layout': go.Layout(
+                    xaxis={'tickangle': 45},
+                    yaxis={'title': '# of classifications'},
+                    margin={'l': 40, 'b': 160, 't': 10, 'r': 100},
+                    legend={'x': 0, 'y': 1},
+#                     hovermode='closest'
+                )
+            }
+
+text_style = dict(color='#444', fontFamily='sans-serif', fontWeight=300)
+head_style = text_style.copy(); head_style.update({'color': '#FFFFFF'})
+
+
+
+
+app.layout = html.Div([
+        html.Div([
+            html.H1('Cloud Classification Training Set Tracker', style=head_style),
+        ], style={'width': '90%', 'height': '100px', 'background-image': 'url(/assets/MODIS_img.jpg)', 'background-size': '100%', 'margin': 'auto', 'text-align': 'center'}),
+        
+        html.Div([
+            html.H3('Summary Statistics', style=text_style),
+            html.P('Total progress on generation classification training data', style=text_style),
+            dcc.Graph(figure=overview_figure),
+        ], style={'width':  '49%', 'vertical-align': 'top', 'display': 'inline-block'}),
+    
+    
+        html.Div([
+            html.H3('User Statistics', style=text_style),
+            dcc.Dropdown(id='user_select', placeholder='Select a user',
+                         options=[{'label': x, 'value': x} for x in users]),
+#             html.P(id='user_header', style=text_style),
+            dcc.Graph(id='user_count_graph'),
+        ], style={'width':  '49%', 'vertical-align': 'top', 'display': 'inline-block'}),
+    ])
+
+
+# @app.callback(Output('user_header', 'children'), [Input('user_select', 'value')])
+# def user_select_callback(text_input):
+#     return 'Showing classification results for <b>{}</b>.'.format(text_input)
+
+@app.callback(Output('user_count_graph', 'figure'), [Input('user_select', 'value')])
+def user_graph_callback(text_input):
+    user_counts = data[data.user_name == text_input].groupby('label').count().annotations
+    figure={
+                'data': [
+                    go.Bar(
+                        x=user_counts.index.values,
+                        y=user_counts.values,
+#                         text=df[df['continent'] == i]['country'],
+#                         mode='markers',
+#                         opacity=0.7,
+#                         marker={
+#                             'size': 15,
+#                             'line': {'width': 0.5, 'color': 'white'}
+#                         },
+#                         name=i
+                    ) #for i in df.continent.unique()
+                ],
+                'layout': go.Layout(
+#                     xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                    yaxis={'title': '# of classifications'},
+                    margin={'l': 40, 'b': 160, 't': 10, 'r': 100},
+                    legend={'x': 0, 'y': 1},
+#                     hovermode='closest'
+                )
+            }
+    return figure
+
+if __name__ == '__main__':
+    
+    if deploy:
+        app.run_server()
+    else:
+        app.run_server(debug=True)
+
